e7d9725f21df896e1183b3ca96c5f81fe0119998
Aurel Pintea <insanovation@gmail.com>
2022-01-25 09:20:41 +0000

    Heap overview.

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"container/heap"
 	"fmt"
 )
 
@@ -14,4 +15,13 @@ func main() {
 	var cube int
 	square, cube, _ = TupleOverview(3)
 	fmt.Println("Square result:", square, "\nCube result:", cube)
+
+	// heap-overview
+	var intHeap *IntegerHeap = &IntegerHeap{-7, 1, 4, 5}
+	heap.Init(intHeap)
+	heap.Push(intHeap, 2)
+	fmt.Printf("minimum: %d\n", (*intHeap)[0])
+	for intHeap.Len() > 0 {
+		fmt.Printf("%d \n", heap.Pop(intHeap))
+	}
 }
