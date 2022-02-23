29e619724819fde33be2d42ee34524c5678e009e
Aurel Pintea <insanovation@gmail.com>
2022-02-23 10:14:49 +0000

    [go-specific-data-structures] DB operations: get Customers.

--- go-specific-api/crud-web-forms/db_operations/crm_database_operations.go
+++ go-specific-api/crud-web-forms/db_operations/crm_database_operations.go
@@ -53,5 +53,37 @@ func GetCustomerById(customerId int) Customer {
 		customer.SSN = SSN
 	}
 
+	defer database.Close()
 	return customer
+}
+
+// GetCustomers method returns Customer Array
+func GetCustomers() []Customer {
+	var database *sql.DB
+	database = GetConnection()
+	var error error
+	var rows *sql.Rows
+	rows, error = database.Query("SELECT * FROM Customer ORDER BY Customerid DESC")
+	if error != nil {
+		panic(error.Error())
+	}
+	var customer Customer
+	customer = Customer{}
+	var customers []Customer
+	customers = []Customer{}
+	for rows.Next() {
+		var customerId int
+		var customerName string
+		var ssn string
+		error = rows.Scan(&customerId, &customerName, &ssn)
+		if error != nil {
+			panic(error.Error())
+		}
+		customer.CustomerId = customerId
+		customer.CustomerName = customerName
+		customer.SSN = ssn
+		customers = append(customers, customer)
+	}
+	defer database.Close()
+	return customers
 }
